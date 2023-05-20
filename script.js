d9c5a8262f0917f3796a916090f4fd04906335c3
jnguye79 <jnguye79@gmail.com>
2023-05-20 17:43:30 -0400

    05/20/23 - TItle Filter Implementation

--- script.js
+++ script.js
@@ -1,66 +1,52 @@
 /* Script used in Posts.html and Portfolio.html */
 
-data_global = null;
+let data_global = null;
 
-/* Update fetch function here... */
+function filterTitleList(array, filterInputValue) {
+    var filtered = [];
+    for (var i = 0; i < array.length; i++) {
+      var unfiltered = array[i];
+      if (unfiltered.title.toLowerCase().includes(filterInputValue.toLowerCase())) {
+        filtered.push(unfiltered);
+      }
+    }
+    return filtered;
+  }
+
+/* Rework to add fetch and catch to handle errors during asynchronous moments. Understand the concept!... */
 const fetchData = () => {
-  fetch('data.json')
-  .then((response) => response.json())
-  .then((data) => {
-    
-    // Access the 'posts' array
-    const posts = data.posts;
-
-    // Access the properties of each post
-    posts.forEach((post) => {
-      const id = post.id;
-      const type = post.type;
-      const startDate = post['date-start'];
-      const endDate = post['date-end'];
-      const dateCreated = post['date-created'];
-      const title = post.title;
-      const alt = post.alt;
-      const thumbnail = post.thumbnail;
-      const caption = post.caption;
-      const description = post.description;
-      const images = post.images;
-      const links = post.links;
-
-      // Perform any desired actions with the post data
-      console.log(id, title, caption, images);
-    });
-
-    // Access the 'portfolio' array
-    const portfolioItems = data.portfolio;
-
-    // Access the properties of each portfolio item
-    portfolioItems.forEach((item) => {
-      // Access the properties of each portfolio item and perform actions
-    });
-
-    // Continue with your code logic here
-  })
-  .catch((error) => {
-    console.log('Error fetching data:', error);
-  });
-};
+    return fetch('data.json')
+       .then((response) => /*{
+        if (!response.ok) {
+          throw new Error('Error fetching data: ' + response.status);
+        }
+        return*/ response.json()
+      )
+      .then((data) => {
+        data_global = data;
+        return data;
+      })
+      /*.catch((error) => {
+        console.log('Error fetching data:', error);
+      });*/
+  };
 
 async function mainEvent() {
-    const nameField = document.querySelector("#Name");
+    const titleField = document.querySelector("#Title");
     const typeField = document.querySelector("Type");
 
-    fetchData();
-    displayList();
+    await fetchData();
+    displayList(data_global.posts);
     /* Continue edits here... */
     
-/*
-    nameField.addEventListener("input", (event) => {
-        console.log("input", event.target.value);
-        const newList = filterList(global_val, event.target.value);
-        console.log("Loading New List...");
-        display(newList);
-    })
 
+    titleField.addEventListener("input", (event) => {
+        console.log("input", event.target.value);
+        const newList = filterTitleList(data_global.posts, event.target.value);
+        console.log("Loading New List...");
+        displayList(newList);
+    })
+/*
     typeField.addEventListener("input", (event) => {
         console.log("input", event.target.value);
         const newList = filterList(global_val, event.target.value);
@@ -70,16 +56,16 @@ async function mainEvent() {
 }
 
 /* Update display content here... */
-const displayList = (pokemon) => {
-    console.log(pokemon);
-    const dataHTMLString = pokemon.map((pokeman) =>
+const displayList = (data) => {
+    console.log(data);
+    const dataHTMLString = data.map((dataList) =>
     `
         <li>
             <div class="card"><a href="info_page.html">
-            <img src="${newList.thumbnail}" title="${newList.thumbnail}">
+            <img src="${dataList.thumbnail}" title="${dataList.thumbnail}">
             <div class="container">
-            <h4><b>${newList.title}</b></h4>
-            ${newList.caption}
+            <h4><b>${dataList.title}</b></h4>
+            ${dataList.caption}
             </a></div>
         </li>
     `
